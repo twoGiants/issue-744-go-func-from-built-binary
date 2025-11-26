bf27bc44d8f09074ad8d4901385caf47b43c5d63
Stanislav Jakuschevskij <sjakusch@redhat.com>
2025-11-26 10:33:14 +0100

    feat: shorten hello word message

--- handle.go
+++ handle.go
@@ -20,7 +20,7 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	msg := "Hello Knative Func! Modified!"
+	msg := "Hello Knative Func!"
 	fmt.Println("Received request")
 	fmt.Println(msg)
 	fmt.Printf("%q\n", dump)
